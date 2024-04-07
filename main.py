52e068c5955d4c1b46b27c51969e942c9938ce88
jerfara1 <>
2024-04-07 11:15:55 -0400

    gitignore

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+Struggling Shoppers
+
+Puzzle game made in Tkinter about purchasing tech products
+
+Jeffrey Zang - ICS3UI final game project
+
+Jan 16 - Jan 30, 2023
+"""
 import tkinter
 import random
 from PIL import Image,ImageTk
@@ -5,10 +14,11 @@ from PIL import Image,ImageTk
 window = tkinter.Tk()
 
 canvas = tkinter.Canvas(window, width=600, height=650)
+# create the canvas
 
 def setInitialValues():
-	"""sets inital variable values"""
-	global size, square, vertical, horizontal, boxes, walls, winx, winy, createdBoxes, createdWalls, score, boxColour, menuItems, font, level1, level2, level3, selectedLevel
+	"""ran at beginning of game, sets inital variable values"""
+	global size, square, vertical, horizontal, boxes, walls, winx, winy, createdBoxes, createdWalls, score, boxColour, menuItems, font, level1, level2, level3, selectedLevel, moves
 	size = 600
 	score = 0
 	square = 40
@@ -24,8 +34,9 @@ def setInitialValues():
 	boxColour = "#d1b9b9"
 	font = ('Inter', 20)
 	selectedLevel = None
+	moves = 0
 
-	level1 = [[
+	level1 = [[ # coordinates for boxes and walls of level 1
 		[3, 3], 
 		[3, 11], 
 		[11, 11], 
@@ -58,7 +69,7 @@ def setInitialValues():
 		[14, 6],
 		[14, 7]
 	]]
-	level2 = [[
+	level2 = [[ # coordinates for boxes and walls of level 2
 		[11, 1],
 		[4, 5],
 		[12, 4],
@@ -121,7 +132,7 @@ def setInitialValues():
 
 		[13,12],
 	]]
-	level3 = [[
+	level3 = [[ # coordinates for boxes and walls of level 3
 		[3, 3],
 		[8, 1],
 		[11, 3],
@@ -188,6 +199,13 @@ def setInitialValues():
 		 [10, 14],
 		 [11, 14]
 	]]
+def incrementMoves():
+	"""adds 1 to the move counter and updates the text"""
+	global moves, movesText
+	moves += 1
+	canvas.delete(movesText)
+	movesText = canvas.create_text(150, 650-square/2, text=f'Moves: {moves}', font=font)
+	canvas.update()
 def createLines():
 	"""creates gridlines"""
 	for i in range(int(size/square)):
@@ -198,10 +216,10 @@ def createLines():
 
 def moveUp():
 	"""moves character up, checks for collision"""
-	global me, shoppingcart
+	global me, shoppingcart, moves
 	coords = canvas.coords(me)
 
-	for i in range(len(boxes)):
+	for i in range(len(boxes)): # checks to see if a box needs to move
 		box = canvas.coords(boxes[i][0])
 		
 		if coords[0] == box[0] and box[1] + square == coords[1]:
@@ -227,7 +245,7 @@ def moveUp():
 	if coords[1]-square < 0:
 		return
 
-	for i in range(len(walls)):
+	for i in range(len(walls)): # checks to see if there's a wall in the way
 		wall = canvas.coords(walls[i])
 		if coords[0] == wall[0] and coords[1]-square == wall[1]:
 			return
@@ -235,14 +253,16 @@ def moveUp():
 	canvas.delete(me, shoppingcart)
 	me = canvas.create_rectangle(coords[0], coords[1]-square, coords[2], coords[3]-square, fill='#c1d2db')
 	shoppingcart = canvas.create_image(coords[0]+square/2,coords[1]-square+square/2,image=shoppingcartImg, anchor='center')
+	incrementMoves()
 	canvas.update()
+	# increments the moves and redraws the character
 	
 def moveLeft():
 	"""moves player left"""
 	global me, shoppingcart
 	coords = canvas.coords(me)
 	
-	for i in range(len(boxes)):
+	for i in range(len(boxes)): # checks and moves boxes that are in the way
 		box = canvas.coords(boxes[i][0])
 		
 		if coords[1] == box[1] and box[0] + square == coords[0]:
@@ -269,7 +289,7 @@ def moveLeft():
 	if coords[0]-square <0:
 		return
 
-	for i in range(len(walls)):
+	for i in range(len(walls)): # checks for walls in the way
 		wall = canvas.coords(walls[i])
 		if coords[1] == wall[1] and coords[0]-square == wall[0]:
 			return
@@ -277,14 +297,16 @@ def moveLeft():
 	canvas.delete(me, shoppingcart)
 	me = canvas.create_rectangle(coords[0]-square, coords[1], coords[2]-square, coords[3], fill='#c1d2db')
 	shoppingcart = canvas.create_image(coords[0]-square+square/2,coords[1]+square/2,image=shoppingcartImg, anchor='center')
+	incrementMoves()
 	canvas.update()
+	# increments moves and redraws box
 	
 def moveDown():
 	"""moves player down"""
 	global me, shoppingcart
-	coords = canvas.coords(me)
+	coords = canvas.coords(me) # gets coordinates of player
 
-	for i in range(len(boxes)):
+	for i in range(len(boxes)): # checks box collision
 		box = canvas.coords(boxes[i][0])
 		if coords[0] == box[0] and box[1] - square == coords[1]:
 			if box[3] == size: return
@@ -306,11 +328,12 @@ def moveDown():
 			canvas.delete(boxes[i][1][0])
 			newimage = canvas.create_image(box[0]+square/2,box[1]+square+square/2,image=boxes[i][1][1], anchor='center')
 			boxes[i][1][0] = newimage
+			# moves box if it gets pushed
 
 	if coords[3]+square > size:
 		return
 
-	for i in range(len(walls)):
+	for i in range(len(walls)): # checks for wall collision
 		wall = canvas.coords(walls[i])
 		if coords[0] == wall[0] and coords[1]+square == wall[1]:
 			return
@@ -318,14 +341,16 @@ def moveDown():
 	canvas.delete(me, shoppingcart)
 	me = canvas.create_rectangle(coords[0], coords[1]+square, coords[2], coords[3]+square, fill='#c1d2db')
 	shoppingcart = canvas.create_image(coords[0]+square/2,coords[1]+square+square/2,image=shoppingcartImg, anchor='center')
+	incrementMoves()
 	canvas.update()
+	# increments moves and redraws player
 	
 def moveRight():
 	"""moves player right"""
 	global me, shoppingcart
 	coords = canvas.coords(me)
 
-	for i in range(len(boxes)):
+	for i in range(len(boxes)): # checks for box collision
 		box = canvas.coords(boxes[i][0])
 		
 		if coords[1] == box[1] and box[0] - square == coords[0]:
@@ -352,7 +377,7 @@ def moveRight():
 	if coords[2]+square > size:
 		return
 
-	for i in range(len(walls)):
+	for i in range(len(walls)): # wall collision
 		wall = canvas.coords(walls[i])
 		if coords[1] == wall[1] and coords[0]+square == wall[0]:
 			return
@@ -361,15 +386,18 @@ def moveRight():
 	canvas.delete(shoppingcart)
 	me = canvas.create_rectangle(coords[0]+square, coords[1], coords[2]+square, coords[3], fill='#c1d2db')
 	shoppingcart = canvas.create_image(coords[0]+square+square/2,coords[1]+square/2,image=shoppingcartImg, anchor='center')
+	incrementMoves()
 	canvas.update()
+	# moves shopping cart
 
 def reset():
 	"""resets screen on enter"""
-	global me, shoppingcart, score, scoreText, dollarImg
-	canvas.delete('all')
+	global me, shoppingcart, score, scoreText, dollarImg, movesText, moves
+	canvas.delete('all') # delete everything
 	createLines()
 	me = canvas.create_rectangle(0, 0, square, square, fill='#c1d2db')
 	shoppingcart = canvas.create_image(square/2, square/2, image=shoppingcartImg)
+	# creates lines, the player, and all the boxes
 	boxes.clear()
 	walls.clear()
 	for box in createdBoxes:
@@ -380,13 +408,17 @@ def reset():
 		createdWall = canvas.create_rectangle(wall[0], wall[1], wall[0]+square, wall[1]+square, fill='black')
 		walls.append(createdWall)
 	score = 0
+	moves = 0
 	canvas.delete(scoreText)
+	canvas.delete(movesText)
+	# reset score and moves
 	scoreText = canvas.create_text(480, 650-square/2, text=f'Items bought: {score}/{len(createdBoxes)}', font=font)
 	canvas.create_rectangle(winx-square, winy-square, winx, winy, fill='#089E17')
 	dollarImg = ImageTk.PhotoImage(Image.open('./assets/dollar.png'))
 	canvas.create_image(winx-square/2,winy-square/2,image=dollarImg, anchor='center')
-	canvas.create_text(150, 650-square/2, text='Struggling Shoppers', font=font)
+	movesText = canvas.create_text(150, 650-square/2, text=f'Moves: {moves}', font=font)
 	canvas.update()
+	# dollar sign 
 
 def createObstacles(min, max): 
 	"""randomly generates obstacles"""
@@ -397,15 +429,18 @@ def createObstacles(min, max):
 	monitor = ImageTk.PhotoImage(Image.open('./assets/monitor.png'))
 	watch = ImageTk.PhotoImage(Image.open('./assets/watch.png'))
 	images = [headphones, monitor, watch]
+	# load possible images
 	
 	for i in range(amount):
 		thing = size/square
 		[startX, startY] = [random.randint(0, int(thing-1)) * square, random.randint(0, int(thing-1)) * square]
+		# picks a starting square
 		if startX == startY == 0: amount += 1; continue
 		if [startX, startY] not in createdWalls:
 			createdWalls.append([startX, startY])
 			wall = canvas.create_rectangle(startX, startY, startX+square, startY+square, fill='black')
 			walls.append(wall)
+			# makes the first wall
 			
 			amount = random.randint(4, 5)
 			direction = random.randint(0,1)
@@ -420,6 +455,7 @@ def createObstacles(min, max):
 					createdWalls.append([startX-square*j, startY])
 					wall = canvas.create_rectangle(startX-square*j, startY, startX-square*(j-1), startY+square, fill='black')
 					walls.append(wall)
+					# makes the next walls in a line
 				if direction == 1:
 					if startY-square*amount == 0:
 						break
@@ -433,17 +469,28 @@ def createObstacles(min, max):
 		else: amount += 1; continue
 
 	amount = random.randint(min, max)
-	for i in range(amount):
+	i = 0 # picks amount of boxes to make
+	while i < amount:
 		thing = size/square
 		[x, y] = [random.randint(1, int(thing-2)) * square, random.randint(1, int(thing-2)) * square]
-		if [x, y] not in createdBoxes and [x, y] not in createdWalls:
-			if x == y == 0: amount += 1; continue
-			box = canvas.create_rectangle(x, y, x+square, y+square, fill=boxColour)
-			selectedImage = random.choice(images)
-			createdImage = canvas.create_image(x+square/2,y+square/2,image=selectedImage, anchor='center')
-			boxes.append([box, [createdImage, selectedImage]])
-			createdBoxes.append([[x, y], [createdImage, selectedImage]])
-		else: amount += 1; continue
+
+		good = True
+		if x == y == 0: good = False
+		for j in createdBoxes:
+			if j[0] == [x, y]: good = False
+		for k in createdWalls:
+			if k == [x, y]: good = False
+		if not good:
+			continue
+		# makes sure the box is legal
+		
+		box = canvas.create_rectangle(x, y, x+square, y+square, fill=boxColour)
+		selectedImage = random.choice(images)
+		createdImage = canvas.create_image(x+square/2,y+square/2,image=selectedImage, anchor='center')
+		boxes.append([box, [createdImage, selectedImage]])
+		createdBoxes.append([[x, y], [createdImage, selectedImage]])
+		i += 1
+		# makes the boxes
 
 	for i in walls:
 		coords = canvas.coords(i)
@@ -452,6 +499,7 @@ def createObstacles(min, max):
 			canvas.update()
 			createdWalls.pop(walls.index(i))
 			walls.remove(i)
+	# Cleans up bad walls
 		
 def createLevel(levelBoxes=[], levelWalls=[]):
 	"""given coordinates, creates a level"""
@@ -460,24 +508,29 @@ def createLevel(levelBoxes=[], levelWalls=[]):
 	monitor = ImageTk.PhotoImage(Image.open('./assets/monitor.png'))
 	watch = ImageTk.PhotoImage(Image.open('./assets/watch.png'))
 	images = [headphones, monitor, watch]
-	canvas.delete('all')
+	# loads images
+	canvas.delete('all') # clears everything
 	for i in levelBoxes:
 		box = canvas.create_rectangle(i[0]*square, i[1]*square, i[0]*square+square, i[1]*square+square, fill=boxColour)
 		selectedImage = random.choice(images)
 		createdImage = canvas.create_image(i[0]*square+square/2,i[1]*square+square/2,image=selectedImage, anchor='center')
 		boxes.append([box, [createdImage, selectedImage]])
 		createdBoxes.append([[i[0]*square, i[1]*square], [createdImage, selectedImage]])
+	# created all boxes
 	for i in levelWalls:
 		wall = canvas.create_rectangle(i[0]*square, i[1]*square, i[0]*square+square, i[1]*square+square, fill='black')
 		walls.append(wall)
 		createdWalls.append([i[0]*square, i[1]*square])
+	# created all walls
 	canvas.update()
 	inmenu=False
+	# update state
 def keyPressed(event):
 	"""detect when a key is pressed"""
-	global score, scoreText, totalBoxes, inmenu
+	global score, scoreText, totalBoxes, inmenu, chosenLevel
 	if not inmenu:
 		if score >= totalBoxes:
+			# checks for winning
 			canvas.create_text(size/2, 100, text='You won!', font=font, justify='center', fill='#34c322')
 			canvas.create_text(size/2, 250, text='Press Backspace to return to the menu', font=font, justify='center', fill='#34c322')
 		if event.keysym in ['Up', 'w']:
@@ -488,6 +541,7 @@ def keyPressed(event):
 			moveRight()
 		elif event.keysym in ['Left', 'a']:
 			moveLeft()
+		# movement
 		elif event.keysym == 'BackSpace':
 			canvas.delete('all')
 			inmenu = True
@@ -498,13 +552,15 @@ def keyPressed(event):
 		elif event.keysym == 'Return':
 			reset()
 			return
-	else:
+		# resetting
+	else: # menu controls
 		if event.keysym == 'space':
 			inmenu = False
 			canvas.delete('all')
 			game()
 		if event.keysym in ['1', '2', '3']:
 			game(event.keysym)
+			chosenLevel = event.keysym
 	for i in range(len(boxes)):
 		box = canvas.coords(boxes[i][0])
 		if box[0] + square == winx and box[1] + square == winy:
@@ -512,9 +568,9 @@ def keyPressed(event):
 			canvas.delete(boxes[i][1][0])
 			boxes.pop(i)
 			score += 1
-			canvas.update()
 			canvas.delete(scoreText)
 			scoreText = canvas.create_text(480, 650-square/2, text=f'Items bought: {score}/{totalBoxes}', font=font)
+			canvas.update()
 			break
 
 def createMenu():
@@ -559,7 +615,7 @@ def createMenu():
 	
 def game(level=None):
 	"""Runs game"""
-	global me, boxes, walls, img, shoppingcart, score, scoreText, totalBoxes, shoppingcartImg, dollar, dollarImg
+	global me, boxes, walls, img, shoppingcart, score, scoreText, totalBoxes, shoppingcartImg, dollar, dollarImg, movesText
 	
 	setInitialValues()
 	if not level:
@@ -570,22 +626,27 @@ def game(level=None):
 		createLevel(level2[0], level2[1])
 	elif level =='3':
 		createLevel(level3[0], level3[1])
+	# loads specific levels
 	createLines()
+	print(len(createdBoxes))
 
 	me = canvas.create_rectangle(0, 0, square, square, fill='#c1d2db')
 
 	shoppingcartImg = ImageTk.PhotoImage(Image.open('./assets/shoppingcart.png'))
 	shoppingcart = canvas.create_image(square/2,square/2,image=shoppingcartImg, anchor='center')
+	# creates shopping cart
 
 	canvas.create_rectangle(winx-square, winy-square, winx, winy, fill='#089E17')
 	dollarImg = ImageTk.PhotoImage(Image.open('./assets/dollar.png'))
 	dollar = canvas.create_image(winx-square/2,winy-square/2,image=dollarImg, anchor='center')
+	# creates dollar
 	totalBoxes = len(boxes)
 	scoreText = canvas.create_text(470, 650-square/2, text=f'Items bought: {score}/{totalBoxes}', font=font)
-	canvas.create_text(150, 650-square/2, text='Struggling Shoppers', font=font)
-
+	movesText = canvas.create_text(150, 650-square/2, text=f'Moves: {moves}', font=font)
+	# creates score and move counter
 
 createMenu()
+# creates starting menu
 
 canvas.bind("<Key>", keyPressed)
 
@@ -593,3 +654,5 @@ canvas.pack()
 
 canvas.focus_set()
 window.mainloop()
+
+# keybinds and stuff
